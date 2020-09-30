cde25c28dcc454801ceadcf23ae3a6c09e87d11d
Vo Truong Trung Hieu <votruongtrunghieu@gmail.com>
2020-09-30 20:49:30 +0700

    Fix InfoPage and Page404

--- src/app/components/InfoPage/index.js
+++ src/app/components/InfoPage/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
 
 import Header from '../NavBar/Header';
 import Content from './Content';
-
-import styles from './style.scss';
 
 // To check if users have logined or not. Waiting for Item name.
 const username = JSON.parse(localStorage.getItem('username')) || '';
@@ -13,7 +10,7 @@ const username = JSON.parse(localStorage.getItem('username')) || '';
 
 function HomePage() {
   return (
-    <div className={styles.container}>
+    <div>
       <Header username={username} />
       <Content />
     </div>
--- src/app/components/Page404/Content.js
+++ src/app/components/Page404/Content.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
 
 import ButtonGoBack from '../Button/BtnGoBack';
 
