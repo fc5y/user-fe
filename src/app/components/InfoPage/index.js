5834315389a23392b8e4764aad9523cf6d093b41
Vo Truong Trung Hieu <votruongtrunghieu@gmail.com>
2020-09-30 14:44:47 +0700

    Edit InfoPage

--- src/app/components/InfoPage/index.js
+++ src/app/components/InfoPage/index.js
@@ -7,11 +7,10 @@ import Content from './Content';
 import styles from './style.scss';
 
 // To check if users have logined or not. Waiting for Item name.
-let username = JSON.parse(localStorage.getItem('username'));
+const username = JSON.parse(localStorage.getItem('username')) || '';
 // To set username:
 // localStorage.setItem('username', JSON.stringify(username_value));
 
-if (username === null) username = '';
 function HomePage() {
   return (
     <div className={styles.container}>
