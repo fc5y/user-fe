e499f07c305e1733158b1e7e87b9c2783a8964bd
qtuan1401 <qtuan14012001@gmail.com>
2020-10-08 09:53:25 +0700

    Modify signup page and add new Popup UI

--- src/app/containers/SignupPage/index.js
+++ src/app/containers/SignupPage/index.js
@@ -2,7 +2,7 @@
 import * as React from 'react';
 
 // HOC
-import { withRouter, Link } from 'react-router-dom';
+import { withRouter, Link, Redirect } from 'react-router-dom';
 import withUserNotLogin from '../../../shared/hoc/withUserNotLogin';
 
 // UI
@@ -12,11 +12,18 @@ import LabeledInput from '../../common-ui/LabeledInput';
 import LabeledRadioGroup from '../../common-ui/LabeledRadioGroup';
 import LabeledCheckbox from '../../common-ui/LabeledCheckbox';
 import * as Button from '../../common-ui/Button';
-
+import Popup from '../../common-ui/Popup';
+import styles from './style.scss';
 // Constants
 import { API_PROGRESS } from '../../../shared/constants/index';
 
 import { getErrors, sanitize, hasBlockingError, signupWithData } from './utils';
+
+const POPUP_STATE = {
+  NONE: 0,
+  ERROR: 1,
+  SUCCESS: 2,
+};
 
 function SignupPage({ history }) {
   const [apiProgress, setApiProgress] = React.useState(API_PROGRESS.INIT);
@@ -34,6 +41,10 @@ function SignupPage({ history }) {
     officialStudent: null,
     iAgreeToTerms: null,
   });
+
+  const [popupState, setPopupState] = React.useState(POPUP_STATE.NONE);
+  const [redirectState, setRedirectState] = React.useState(false);
+
   const errors = getErrors(data);
 
   const handleChange = React.useCallback((name, value) => {
@@ -43,25 +54,56 @@ function SignupPage({ history }) {
   const submit = React.useCallback(async () => {
     const sanitizedData = sanitize(data);
     setData(sanitizedData);
-    if (hasBlockingError(getErrors(sanitizedData))) return;
+    // eslint-disable-next-line no-shadow
+    if (hasBlockingError(getErrors(sanitizedData))) {
+      setPopupState(POPUP_STATE.ERROR);
+      return;
+    }
     setApiProgress(API_PROGRESS.REQ);
     // eslint-disable-next-line no-shadow
     const { data: response, error } = await signupWithData(sanitizedData);
 
     if (!!error || !response || !response.username) {
       // TODO: show popup
-      alert(`Đăng ký không thành công\n\n${error}`);
+      setPopupState(POPUP_STATE.ERROR);
       setApiProgress(API_PROGRESS.FAILED);
     } else {
       // TODO: show popup
-      alert(`Đăng ký thành công!`);
       setApiProgress(API_PROGRESS.SUCCESS);
-      !!history && history.push('/login');
+      setPopupState(POPUP_STATE.SUCCESS);
     }
   }, [data]);
 
+  const handleClosePopup = React.useCallback(() => {
+    setPopupState(POPUP_STATE.NONE);
+  });
+
+  const onButtonClick = React.useCallback(() => {
+    setRedirectState(true);
+  });
+
   return (
     <MainPanel.Container>
+      {redirectState === true ? <Redirect to="/login" /> : ''}
+      {popupState === POPUP_STATE.NONE ? (
+        ''
+      ) : popupState === POPUP_STATE.ERROR ? (
+        <Popup
+          onClose={handleClosePopup}
+          title="Đăng ký thất bại"
+          content="Đã xảy ra lỗi trong quá trình đăng ký. Vui lòng kiểm tra lại."
+          buttonText="OK"
+          variant="error"
+        />
+      ) : (
+        <Popup
+          title="Đăng ký thành công"
+          content="Cảm ơn bạn đã đăng ký tham gia FYT Code Cup! Vui lòng đăng nhập để tiếp tục."
+          buttonText="Đăng nhập"
+          variant="success"
+          onButtonClick={onButtonClick}
+        />
+      )}
       <MainPanel.Title>Đăng ký tham gia FYT Code Cup</MainPanel.Title>
       <MainPanel.Description>
         Để tham gia FYT Code Cup, hãy đăng ký trước 19:30 ngày 09/10/2020 bạn nhé!
@@ -166,7 +208,10 @@ function SignupPage({ history }) {
           <LabeledCheckbox
             label={
               <span>
-                Tôi đã đọc và đồng ý với <Link to="/info">quy chế thi của FYT Code Cup</Link>
+                Tôi đã đọc và đồng ý với{' '}
+                <Link className={styles.linkDecoration} to="/info">
+                  quy chế thi của FYT Code Cup
+                </Link>
               </span>
             }
             name="iAgreeToTerms"
