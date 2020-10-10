e37ddd943a6e7d26aae4299b0ba667d0bbe28cdd
Nhan <ngohuynhnhan@gmail.com>
2020-10-10 19:01:55 +0800

    Using ContestInfoContext instead of fetching manually

--- src/app/containers/EnterPage/index.js
+++ src/app/containers/EnterPage/index.js
@@ -1,76 +1,47 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import { Redirect } from 'react-router-dom';
 
 import { UserInfoContext } from '../../../shared/context/UserInfo';
+import { ContestInfoContext } from '../../../shared/context/ContestInfo';
 
 import styles from './enter.scss';
 
-// APIs
-import { apiGetTime } from '../../../api/authentication';
+function EnterPage() {
+  const { contestInfo } = React.useContext(ContestInfoContext);
+  const { userInfo, isFetched: isUserInfoFetched } = React.useContext(UserInfoContext);
 
-class EnterPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isContestStart: null,
-    };
+  if (!isUserInfoFetched || !contestInfo.isFetched) {
+    // Loading/Fetching state
+    return null;
+  } else if ((!!userInfo && userInfo.username === null) || !contestInfo.isContestReady) {
+    // Check if user has login or not. If not redirect back to homepage
+    // Or the contest is not ready, we also need to redirect
+    return <Redirect to="/" />;
   }
 
-  componentDidMount() {
-    const getRegisterTime = async () => {
-      const { data: response, error } = await apiGetTime();
-
-      if (error || (!!response && response.countdownOpen > 0)) {
-        this.setState({ isContestStart: false });
-      } else {
-        this.setState({ isContestStart: true });
-      }
-    };
-
-    if (this.state.isContestStart === null) {
-      getRegisterTime();
-    }
-  }
-
-  render() {
-    if (!this.context || !this.context.isFetched || this.state.isContestStart === null) {
-      // Loading/Fetching state
-      return null;
-    } else if (
-      (!!this.context.userInfo && this.context.userInfo.username === null) ||
-      !this.state.isContestStart
-    ) {
-      // Check if user has login or not. If not redirect back to homepage
-      // Or the contest is not ready, we also need to redirect
-      return <Redirect to="/" />;
-    }
-
-    const { username, contestPassword } = this.context.userInfo;
-    return (
-      <div className={styles.container}>
-        <h5 className={styles.titleContent}>Vào thi</h5>
-        <div className={styles.enterContest}>
-          <h6 className={styles.textDecoration}>
-            1. Truy cập vào địa chỉ:&nbsp;
-            <a className={styles.linkDecoration} href="contest.freecontest.net" target="_blank">
-              contest.freecontest.net
-            </a>
-          </h6>
-        </div>
-
-        <div className={styles.userInfo}>
-          <h6 className={styles.textDecoration}>2. Đăng nhập với thông tin đăng nhập như sau:</h6>
-          <ul className={styles.info}>
-            <li className={styles.lstInfo}>Username: {username}</li>
-            <li className={styles.lstInfo}>Password: {contestPassword}</li>
-          </ul>
-        </div>
+  const { username, contestPassword } = contestInfo;
+  return (
+    <div className={styles.container}>
+      <h5 className={styles.titleContent}>Vào thi</h5>
+      <div className={styles.enterContest}>
+        <h6 className={styles.textDecoration}>
+          1. Truy cập vào địa chỉ:&nbsp;
+          <a className={styles.linkDecoration} href="contest.freecontest.net" target="_blank">
+            contest.freecontest.net
+          </a>
+        </h6>
       </div>
-    );
-  }
-}
 
-Component.contextType = UserInfoContext;
+      <div className={styles.userInfo}>
+        <h6 className={styles.textDecoration}>2. Đăng nhập với thông tin đăng nhập như sau:</h6>
+        <ul className={styles.info}>
+          <li className={styles.lstInfo}>Username: {username}</li>
+          <li className={styles.lstInfo}>Password: {contestPassword}</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
 
 export default EnterPage;
