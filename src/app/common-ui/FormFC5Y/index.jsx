310e917ff7343de9e0672e4c1ecd28076171ea99
Kien Nguyen <kc97ble@gmail.com>
2020-11-21 20:54:04 +0800

    Add signup page

---
+++ src/app/common-ui/FormFC5Y/index.jsx
@@ -0,0 +1,15 @@
+// import * as React from 'react';
+// import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+export const Form = styled.form``;
+
+export const ButtonGroup = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export { default as FieldSet } from './components/FieldSet';
+export { default as LabeledInput } from './components/LabeledInput';
+export { default as LabeledCheckbox } from './components/LabeledCheckbox';
---
+++ src/app/common-ui/FormFC5Y/types.js
@@ -0,0 +1,9 @@
+import PropTypes from 'prop-types';
+
+export const commonInputPropTypes = {
+  label: PropTypes.node.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  error: PropTypes.string,
+  onChange: PropTypes.func,
+};
