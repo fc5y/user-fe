cfc9d5bfd5b046a34b01126bff72f54059da1a0f
qtuan1401 <qtuan14012001@gmail.com>
2020-12-22 22:29:48 +0700

    Change svg to js files

--- src/app/common-ui/ErrorBox/index.js
+++ src/app/common-ui/ErrorBox/index.js
@@ -2,14 +2,14 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 
 // Assets
-import iconError from 'assets/images/error.svg';
+import IconError from 'src/app/common-ui/Icons/IconError';
 
 import styles from './style.scss';
 
 export default function ErrorBox({ content }) {
   return (
     <div className={styles.container}>
-      <img className={styles.icon} src={iconError} alt="error" />
+      <IconError className={styles.icon} />
       <div className={styles.text}>{content}</div>
     </div>
   );
--- src/app/common-ui/Popup/index.js
+++ src/app/common-ui/Popup/index.js
@@ -4,10 +4,12 @@ import styles from './style.scss';
 import PropTypes from 'prop-types';
 
 import CloseButton from 'assets/images/close-button.png';
-import iconError from 'assets/images/error.svg';
-import iconSuccess from 'assets/images/success.svg';
-import iconWarning from 'assets/images/warning.svg';
 import cx from 'classnames';
+
+// common-ui
+import IconError from 'src/app/common-ui/Icons/IconError';
+import IconWarning from 'src/app/common-ui/Icons/IconWarning';
+import IconSuccess from 'src/app/common-ui/Icons/IconSuccess';
 
 export const POPUP_VARIANT = {
   DEFAULT: 0,
@@ -26,10 +28,10 @@ export default function Popup({ show, content, variant, onClose }) {
     }
   }, [show]);
 
-  const selectSign = (variantToIconUrl) => {
-    if (variantToIconUrl === POPUP_VARIANT.ERROR) return iconError;
-    if (variantToIconUrl === POPUP_VARIANT.SUCCESS) return iconSuccess;
-    if (variantToIconUrl === POPUP_VARIANT.WARNING) return iconWarning;
+  const selectSign = (variantToIconUrl, className) => {
+    if (variantToIconUrl === POPUP_VARIANT.ERROR) return <IconError className={className} />;
+    if (variantToIconUrl === POPUP_VARIANT.SUCCESS) return <IconSuccess className={className} />;
+    if (variantToIconUrl === POPUP_VARIANT.WARNING) return <IconWarning className={className} />;
     return '';
   };
 
@@ -40,9 +42,7 @@ export default function Popup({ show, content, variant, onClose }) {
           <div className={styles.closeImg} onClick={onClose}>
             <img src={CloseButton} alt="close" />
           </div>
-          <div className={styles.iconContainer}>
-            <img className={styles.icon} src={selectSign(variant)} alt="popupSign" />
-          </div>
+          <div className={styles.iconContainer}>{selectSign(variant, styles.icon)}</div>
           <div className={styles.content}>{content}</div>
         </div>
       </div>,
