68f41e9174ea17debf3a468e8e653512da6e3706
Nhan <ngohuynhnhan@gmail.com>
2020-12-22 13:11:39 +0800

    Remove unused parts in HomePage

--- src/app/containers/HomePage/index.js
+++ src/app/containers/HomePage/index.js
@@ -1,59 +1,28 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
-// Utils
+// Context
 import { UserInfoContext } from 'src/shared/context/UserInfo';
 import { ContestInfoContext } from 'src/shared/context/ContestInfo';
 
-// Components
-import Markdown from 'react-markdown';
-import { Link } from 'react-router-dom';
-import BtnLoginAndSignup from '../../components/Button/BtnLoginAndSignup';
-import BtnJoin from '../../components/Button/BtnJoin';
-import BtnDisabled from '../../components/Button/BtnDisabled';
-import Footer from 'src/app/components/Footer';
+// Utils
+import styled from 'styled-components';
 
-// Data
-import md from './Info.md';
-
-import styles from './style.scss';
+const Container = styled.div`
+  width: 100%;
+  margin: 48px 0;
+`;
 
 function HomePage() {
   const { userInfo } = React.useContext(UserInfoContext);
   const { contestInfo } = React.useContext(ContestInfoContext);
 
   return (
-    <div>
-      <div className={styles.content}>
-        <Helmet>
-          <title>Cổng đăng ký FYT Code Cup</title>
-        </Helmet>
-        <div className={styles.title}>FYT Code Cup</div>
-        <div className={styles.info}>
-          <Markdown source={md} />
-          <Link to="/info">Quy chế của kỳ thi</Link>
-        </div>
-        {!userInfo || !userInfo.username ? (
-          <>
-            <div className={styles.alert}>Để tham gia thi, bạn cần tạo tài khoản</div>
-            <BtnLoginAndSignup />
-          </>
-        ) : contestInfo.isContestReady ? (
-          <>
-            <div className={styles.alert}>Để tham gia thi, bạn chỉ cần nhấn vào nút “Vào thi”</div>
-            <BtnJoin />
-          </>
-        ) : (
-          <>
-            <div className={styles.alert}>
-              Trang kỳ thi sẽ chỉ được bật một ít phút trước khi kỳ thi bắt đầu
-            </div>
-            <BtnDisabled />
-          </>
-        )}
-      </div>
-      <Footer />
-    </div>
+    <Container>
+      <Helmet>
+        <title>Freecontest</title>
+      </Helmet>
+    </Container>
   );
 }
 
