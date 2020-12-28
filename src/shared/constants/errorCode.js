a156551195927ad67ec40fe29119089c79262cc4
Nhan <ngohuynhnhan@gmail.com>
2020-12-28 21:33:47 +0800

    Add error code for OTP exceed limit

--- src/shared/constants/errorCode.js
+++ src/shared/constants/errorCode.js
@@ -13,8 +13,13 @@ export const API_ERROR = Object.freeze({
   EMAIL_EXISTED: 3003,
   INVALID_TOKEN: 3005,
   WRONG_CRED: 3006,
+
+  // OTP
   OTP_INVALID: 3008,
   OTP_EXPIRED: 3009,
+  OTP_EXCEED_SEND_LIMIT: 3010,
+  OTP_EXCEED_SEND_LIMIT_SYSTEM: 3011,
+  OTP_EXCEED_VERIFY_LIMIT: 3012,
 
   // Constest
   CONTEST_NOT_FOUND: 2101,
@@ -33,14 +38,21 @@ export const ERROR_MAP = Object.freeze({
   [API_ERROR.DATABASE_ERROR]: 'Đã có lỗi xảy ra, vui lòng thử lại.',
   [API_ERROR.INVALID_ENDPOINT]: 'Đã có lỗi xảy ra, vui lòng thử lại.',
   [API_ERROR.VALIDATION_ERRORS]: 'Đã có lỗi xảy ra, vui lòng thử lại.',
-  [API_ERROR.INVALID_TOKEN]: 'Đã có lỗi xảy ra, vui lòng đăng nhập lại.',
 
   // Auth
   [API_ERROR.WRONG_CRED]: 'Tên đăng nhập/email hoặc mật khẩu không hợp lệ. Vui lòng thử lại.',
   [API_ERROR.EMAIL_EXISTED]: 'Email đã được sử dụng.',
   [API_ERROR.USERNAME_EXISTED]: 'Tên đăng nhập đã được sử dụng.',
+  [API_ERROR.INVALID_TOKEN]: 'Đã có lỗi xảy ra, vui lòng đăng nhập lại.',
+
+  // OTP
   [API_ERROR.OTP_INVALID]: 'Mã xác nhận không đúng',
   [API_ERROR.OTP_EXPIRED]: 'Mã xác nhận đã hết hạn, vui lòng đăng ký lại.',
+  [API_ERROR.OTP_EXCEED_SEND_LIMIT]:
+    'Bạn (hoặc ai đó có cùng địa chỉ IP) đã gửi yêu cầu quá nhiều lần. Vui lòng thử lại sau.',
+  [API_ERROR.OTP_EXCEED_VERIFY_LIMIT]:
+    'Bạn (hoặc ai đó có cùng địa chỉ IP) đã gửi yêu cầu quá nhiều lần. Vui lòng thử lại sau.',
+  [API_ERROR.OTP_EXPIRED]: 'Dịch vụ gửi email đang quá tải. Vui lòng thử lại sau.',
 
   // Constest
   [API_ERROR.CONTEST_NOT_FOUND]: 'Kỳ thi không tồn tại.',
