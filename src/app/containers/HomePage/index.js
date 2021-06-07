3cdf93e9f2cc91ebf2f5e79a3cc56982f3d690ac
Phạm Ngọc Thùy Trang <43264944+PikarryPham@users.noreply.github.com>
2021-06-07 20:58:30 +0700

    fix hmepage banner final ver

--- src/app/containers/HomePage/index.js
+++ src/app/containers/HomePage/index.js
@@ -1,7 +1,5 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet';
-import cx from 'classnames';
-import styles from './style.scss';
 
 // Common UI
 import AlertWarning from 'src/app/common-ui/AlertWarning';
@@ -17,17 +15,11 @@ import OngoingContest from 'src/app/components/ContestsOngoing';
 import EndedContests from 'src/app/components/ContestsEnded';
 import WelcomeBanner from './components/WelcomeBanner';
 import Footer from 'src/app/components/Footer';
+import HomePageBanner from './components/HomePageBanner';
 
 // Utils and constants
 import styled from 'styled-components';
 import { API_PROGRESS } from 'src/shared/constants';
-
-// Assets
-import bannerImage from 'assets/images/home_banner.png';
-import IconBanner from 'src/assets/images/bannerlogo.png';
-import IconUsers from 'src/assets/images/icon-users.png';
-import IconMedal from 'src/assets/images/icon-medal.png';
-import IconFC5Y from 'src/assets/images/icon-fc.png';
 
 const Container = styled.div`
   width: 100%;
@@ -98,38 +90,7 @@ function HomePage() {
           contests={onGoingContests}
         />
         <BannerImageWrapper>
-          <div className={cx(styles.container)}>
-            <img className={cx(styles.image)} src={IconBanner} alt="homepage-banner" />
-            <div className={cx(styles.container_child)}>
-              <div className={cx(styles.container_child_logo)}>
-                <img src={IconFC5Y} alt="icon-logoFC5Y"></img>
-                <div className={cx(styles.container_child_cnt)}>
-                  <p>
-                    <b>5 năm</b>
-                  </p>
-                  <p>Tổ chức các kỳ thi</p>
-                </div>
-              </div>
-              <div className={cx(styles.container_child_users)}>
-                <img src={IconUsers} alt="icon-users"></img>
-                <div className={cx(styles.container_child_cnt)}>
-                  <p>
-                    <b>3800+</b>
-                  </p>
-                  <p>Thành viên</p>
-                </div>
-              </div>
-              <div className={cx(styles.container_child_medal)}>
-                <img src={IconMedal} alt="icon-medal"></img>
-                <div className={cx(styles.container_child_cnt)}>
-                  <p>
-                    <b>Nền tảng lập trình thi đấu</b>
-                  </p>
-                  <p>hàng đầu Việt Nam</p>
-                </div>
-              </div>
-            </div>
-          </div>
+          <HomePageBanner></HomePageBanner>
         </BannerImageWrapper>
         {userInfo.isFetched && !userInfo.username && <WelcomeBanner />}
         <EndedContests
