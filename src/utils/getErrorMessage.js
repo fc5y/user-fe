6d4024c3bb71194d7e29ce641ede2d46d9a94902
ndhuy21 <huytinhoc21@gmail.com>
2021-09-30 19:38:08 +0700

    add-error-code-to-geterrormessage

--- src/utils/getErrorMessage.js
+++ src/utils/getErrorMessage.js
@@ -1,9 +1,7 @@
 import { ERROR_MAP, API_ERROR } from '../shared/constants';
 
-export function getErrorMessage({ error, error_msg }) {
-  if (!error && !error_msg) {
-    return ERROR_MAP[API_ERROR.UNKNOWN_ERROR];
-  }
-
-  return ERROR_MAP[error] || error_msg;
+export function getErrorMessage({ error, error_msg }, showErrorCode = true) {
+  const message =
+    !error && !error_msg ? ERROR_MAP[API_ERROR.UNKNOWN_ERROR] : ERROR_MAP[error] || error_msg;
+  return message + (showErrorCode ? `(${error})` : '');
 }
