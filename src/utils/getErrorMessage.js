17cd2ddf34c9ce0cd2a4dc02e686911f225850b1
Huynh Nhan Ngo <ngohuynhnhan@gmail.com>
2021-10-01 08:59:54 -0700

    Add one more space next to the error code

--- src/utils/getErrorMessage.js
+++ src/utils/getErrorMessage.js
@@ -3,5 +3,5 @@ import { ERROR_MAP, API_ERROR } from '../shared/constants';
 export function getErrorMessage({ error, error_msg }, showErrorCode = true) {
   const message =
     !error && !error_msg ? ERROR_MAP[API_ERROR.UNKNOWN_ERROR] : ERROR_MAP[error] || error_msg;
-  return message + (showErrorCode ? `(${error})` : '');
+  return message + (showErrorCode ? ` (${error})` : '');
 }
